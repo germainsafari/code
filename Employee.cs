5719e5e8617eb557347e5f8af83dad36c060b3cf
Safari Germain <99614251+germainsafari@users.noreply.github.com>
2024-01-01 14:10:59 +0100

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -1,1 +1,17 @@
-
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+ 
+[Serializable]
+abstract class Employee
+{
+    public string name;
+    public string surname;
+    public string pesel;
+    public string username;
+    public string password;
+ 
+    public abstract void DisplayInfo();
+}
